7651ff4b237ff3102b322c1ca54463a5fa6f77a3
Odd-Arne Johansen <odd-arne.johansen@cerpus.com>
2019-05-02 09:07:57 +0200

    Use the correct route for deletion status

--- routes/gdpr.php
+++ routes/gdpr.php
@@ -7,7 +7,7 @@ Route::group([
 ], function () {
     Route::post('/api/gdpr/delete', 'GdprDeleteController@store')->name('gdpr.store');
     Route::get('/api/gdpr/delete/status', 'GdprDeleteController@index')->name('gdpr.index');
-    Route::get('/api/gdpr/{id}/status', 'GdprDeleteController@show')->name('gdpr.show');
+    Route::get('/api/gdpr/delete/{id}/status', 'GdprDeleteController@show')->name('gdpr.show');
 });
 
 
